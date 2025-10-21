44bef7107f59949c767c59f8b94f49783a4a9c01
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-10-21 17:25:02 -0300

    fix: process.env not have environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,16 +1,27 @@
-import { z } from "zod";
+import { z } from 'zod';
+import { FormatZodErrors } from '../utils/funcs/FormatZodErrors';
 
 const envSchema = z.object({
-    PORT: z.coerce.number()
-        .positive('Deve ser positivo')
-        .default(3000)
-})
+	DATABASE_URL: z.string('Informe a url de conexão com o banco de dados'),
+	PORT: z.coerce.number().positive('Deve ser positivo').default(3000),
+	ASAAS_API_KEY: z
+		.string(
+			'Por favor informe sua api key do assas (recomendo usar uma api key sandbox)',
+		)
+		.min(
+			1,
+			'Por favor informe sua api key do assas (recomendo usar uma api key sandbox)',
+		),
+});
 
 const parsedEnv = envSchema.safeParse(process.env);
 
 if (!parsedEnv.success) {
-  console.error("Erro nas variáveis de ambiente:", parsedEnv.error.format());
-  process.exit(1);
+	console.error(
+		'Erro nas variáveis de ambiente:',
+		FormatZodErrors(parsedEnv.error),
+	);
+	process.exit(1);
 }
 
 export const env = parsedEnv.data;
