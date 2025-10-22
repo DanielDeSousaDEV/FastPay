dfc86dd76d4aa2f557003f5866f3ba8f0e29b5fa
DanielDeSousaDEV <danieldesousa.dev@gmail.com>
2025-10-22 15:40:50 -0300

    chore: making errors messages better

--- src/utils/validators/charges.ts
+++ src/utils/validators/charges.ts
@@ -1,27 +1,25 @@
 import z from 'zod';
-import { validateCPF } from '../funcs/ValidateCPF';
-import { validateCNPJ } from '../funcs/ValidateCNPJ';
 import { PaymentType } from '@prisma/client';
 
 export const ChargeIdSchema = z.object({
 	id: z.coerce
-		.number('Por favor informe o id do cliente')
-		.int('Por favor informe o id do cliente')
-		.min(1, 'Por favor informe o id do cliente')
+		.number('Por favor informe o id da combrança')
+		.int('Por favor informe o id da combrança')
+		.min(1, 'Por favor informe o id da combrança')
 		.positive('Por favor informe o id valído'),
 });
 
 const CreateChargeRequestBaseFields = {
 	amount: z
-		.number('Por favor informe o valor  do cliente')
-		.positive('Por favor informe o nome do cliente'),
+		.number('Por favor informe o valor da combrança')
+		.positive('Por favor informe o valor da combrança'),
 	currency: z
-		.string('Por favor informe o email do cliente')
-		.min(1, 'Por favor informe o email do cliente')
-		.max(255, 'O email do cliente deve ter no máximo 255 caracteres'),
+		.string('Por favor informe a moeda da combrança')
+		.min(1, 'Por favor informe a moeda da combrança')
+		.max(255, 'A moeda da combrança deve ter no máximo 255 caracteres'),
 	customerId: z
-		.int('Por favor informe o documento do cliente')
-		.positive('Por favor informe o documento do cliente'),
+		.int('Por favor informe cliente da combrança')
+		.positive('Por favor informe cliente da combrança'),
 	dueDate: z.iso.date('Por favor informe a data de validade da combrança'),
 };
 
@@ -53,30 +51,45 @@ export const CreateChargeRequest = z.discriminatedUnion(
 	'Informe um metodo de pagamento valído',
 );
 export type CreateChargeRequest = z.infer<typeof CreateChargeRequest>;
+const UpdateChargeRequestBaseFields = {
+	amount: z
+		.number('Por favor informe o valor da combrança')
+		.positive('Por favor informe o valor da combrança'),
+	currency: z
+		.string('Por favor informe a moeda da combrança')
+		.min(1, 'Por favor informe a moeda da combrança')
+		.max(255, 'A moeda da combrança deve ter no máximo 255 caracteres'),
+	customerId: z
+		.int('Por favor informe cliente da combrança')
+		.positive('Por favor informe cliente da combrança'),
+	dueDate: z.iso.date('Por favor informe a data de validade da combrança'),
+};
 
-export const UpdateCostumerRequest = z.object({
-	name: z
-		.string('Por favor informe o nome do cliente')
-		.min(1, 'Por favor informe o nome do cliente')
-		.max(255, 'O nome do cliente deve ter no máximo 255 caracteres')
-		.optional(),
-	email: z
-		.email('Por favor informe o email do cliente')
-		.min(1, 'Por favor informe o email do cliente')
-		.max(255, 'O email do cliente deve ter no máximo 255 caracteres')
-		.optional(),
-	document: z
-		.string('Por favor informe o documento do cliente')
-		.min(1, 'Por favor informe o documento do cliente')
-		.max(255, 'O email do cliente deve ter no máximo 255 caracteres')
-		.regex(
-			/^(?:\d{3}\.\d{3}\.\d{3}-\d{2}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2})$/,
-			'Informe CPF (000.000.000-00) ou CNPJ (00.000.000/0000-00) com formato valído',
-		)
-		.refine(
-			(val) => validateCPF(val) || validateCNPJ(val),
-			'Informe CPF (000.000.000-00) ou CNPJ (00.000.000/0000-00) valído',
-		)
-		.optional(),
+const UpdatePixChargeRequest = z.object({
+	...UpdateChargeRequestBaseFields,
+	paymentType: z.literal(PaymentType.PIX),
 });
-export type UpdateCostumerRequest = z.infer<typeof UpdateCostumerRequest>;
+
+const UpdatePaymentSlipChargeRequest = z.object({
+	...UpdateChargeRequestBaseFields,
+	paymentType: z.literal(PaymentType.PAYMENT_SLIP),
+});
+
+const UpdateCreditCardChargeRequest = z.object({
+	...UpdateChargeRequestBaseFields,
+	paymentType: z.literal(PaymentType.CREDIT_CARD),
+	installments: z
+		.int('A quantidade de parcelas deve ser um número')
+		.positive('A quantidade de parcelas deve ser um número positivo'),
+});
+
+export const UpdateChargeRequest = z.discriminatedUnion(
+	'paymentType',
+	[
+		UpdateCreditCardChargeRequest,
+		UpdatePaymentSlipChargeRequest,
+		UpdatePixChargeRequest,
+	],
+	'Informe um metodo de pagamento valído',
+);
+export type UpdateChargeRequest = z.infer<typeof UpdateChargeRequest>;
